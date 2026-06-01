94f54260dac16ac872052294552b0d46da046729
sandrp <sandro@sandrp.de>
2026-06-02 01:20:52 +0200

    fixed Discord Server name Syntax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -193,7 +193,7 @@ export default function Home() {
                     <span className="text-zinc-500 mr-4">[matrix]</span> @me:sandrp.de
                   </a>
                   <a href="https://discord.gg/32tpYxetAu" className="hover:text-[#bd4954] transition-colors w-max">
-                    <span className="text-zinc-500 mr-4">[dc-server]</span> "Water System"
+                    <span className="text-zinc-500 mr-4">[dc-server]</span> {"\"Water System\""}
                   </a>
                 </div>
               </Window>
